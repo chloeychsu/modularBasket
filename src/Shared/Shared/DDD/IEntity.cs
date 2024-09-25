b7b24e202a77b3902352a11a2500c08d526708ea
chloeychsu <chloe.yc.hsu@gmail.com>
2024-09-25 16:00:36 +0800

    feat: added Domain Driven Design and Domain Event Pattern to the project.

---
+++ src/Shared/Shared/DDD/IEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.DDD;
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public string? CreateBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
