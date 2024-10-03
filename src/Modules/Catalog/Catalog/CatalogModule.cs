a17a5d965d2f74c630a70b72c5639ddb53c6a8a6
chloeychsu <chloe.yc.hsu@gmail.com>
2024-10-03 17:08:21 +0800

    feat:add basketdb connection string to API project,migrate catalog schema with Products table,seeding data into Products table.

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -2,16 +2,30 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace Catalog;
 
 public static class CatalogModule
 {
     public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
     {
+        // Add services to the container
+        // API Endpoint services
+        // Application use case services
+        // Infrastructure services
+        services.AddDbContext<CatalogDbContext>(option =>
+            {
+                // option.AddInterceptors(new AuditableEntityInterceptor());
+                option.UseNpgsql(configuration.GetConnectionString("Database"));
+            }
+         );
+        // services.AddScoped<IDataSeeder, CatalogDataSeeder>();
         return services;
     }
     public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
     {
+        // Use Data - Infrastructure services
+        app.UseMigration<CatalogDbContext>();
         return app;
     }
 }
