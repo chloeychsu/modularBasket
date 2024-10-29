2877c136f24ef9b9c2c60a5a3f30f7b9413fd8a4
chloeychsu <chloe.yc.hsu@gmail.com>
2024-10-29 11:22:50 +0800

    feat:create domain event handler for catalog

---
+++ src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog;
+
+public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger) : INotificationHandler<ProductChangedEvent>
+{
+     public Task Handle(ProductChangedEvent notification, CancellationToken cancellationToken)
+     {
+          logger.LogInformation("Domain Event handler: {DomainEvent}", notification.GetType().Name);
+          return Task.CompletedTask;
+     }
+}
