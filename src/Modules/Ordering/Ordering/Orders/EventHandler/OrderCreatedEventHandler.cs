19292281debf1f2a07e1bbcf8400039539157366
chloeychsu <chloe.yc.hsu@gmail.com>
2024-11-17 13:43:24 +0800

    [feat]:add ordering domain with DDD,create domain event and event handler

---
+++ src/Modules/Ordering/Ordering/Orders/EventHandler/OrderCreatedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Ordering.Orders.EventHancler;
+
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Domain Event handled:{notification.GetType().Name}");
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Modules/Ordering/Ordering/Orders/Modules/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Orders.Modules;
+
+public class OrderItem:Entity<Guid>
+{
+    public Guid OrderId { get; private set; } = default!;
+    public Guid ProductId { get; private set; } = default!;
+    public int Quantity { get; internal set; } = default!;
+    public decimal Price { get; private set; } = default!;
+    public OrderItem(Guid orderId,Guid productId,int quantity,decimal price)
+    {
+        OrderId = orderId;
+        ProductId = productId;
+        Quantity = quantity;
+        Price = price;
+    }
+}
