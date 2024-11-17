9b155fd1134a441bb272266689e3511540f298e5
chloeychsu <chloe.yc.hsu@gmail.com>
2024-11-17 18:18:43 +0800

    [feat]:create outbox table and migrate DB,insert outbox table before delete checlout basket data

--- src/Modules/Basket/Basket/Basket/Features/CheckoutBasket/CheckoutBasketHandler.cs
+++ src/Modules/Basket/Basket/Basket/Features/CheckoutBasket/CheckoutBasketHandler.cs
@@ -1,4 +1,4 @@
-﻿using MassTransit;
+﻿using System.Text.Json;
 using Shared.Messaging.Events;
 
 namespace Basket.Basket.Features.CheckoutBasket;
@@ -15,15 +15,52 @@ public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCo
     }
 }
 
-public class CheckoutBasketHandler(IBasketRepository repository, IBus bus) : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+public class CheckoutBasketHandler(BasketDbContext dbContext) : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
 {
     public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
     {
-        var basket = await repository.GetBasket(command.BasketCheckout.UserName, true, cancellationToken);
-        var eventMessage = command.BasketCheckout.Adapt<BasketCheckoutIntegrationEvent>();
-        eventMessage.TotalPrice = basket.TotalPrice;
-        await bus.Publish(eventMessage,cancellationToken);
-        await repository.DeleteBasket(command.BasketCheckout.UserName,cancellationToken);
-        return new CheckoutBasketResult(true);
+        ////// Checkout Basket Without Outbox ---- START
+        // var basket = await repository.GetBasket(command.BasketCheckout.UserName, true, cancellationToken);
+        // var eventMessage = command.BasketCheckout.Adapt<BasketCheckoutIntegrationEvent>();
+        // eventMessage.TotalPrice = basket.TotalPrice;
+        // await bus.Publish(eventMessage,cancellationToken);
+        // await repository.DeleteBasket(command.BasketCheckout.UserName,cancellationToken);
+        // return new CheckoutBasketResult(true);
+        ////// Checkout Basket Without Outbox ---- END
+        ///
+
+        await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+        try
+        {
+            // 取得當前在購物車的物品和總金額
+            var basket = await dbContext.ShoppingCarts.Include(x => x.Items).SingleOrDefaultAsync(x => x.UserName == command.BasketCheckout.UserName, cancellationToken);
+            if (basket == null)
+            {
+                throw new BasketNotFoundException(command.BasketCheckout.UserName);
+            }
+            var eventMessage = command.BasketCheckout.Adapt<BasketCheckoutIntegrationEvent>();
+            eventMessage.TotalPrice = basket.TotalPrice;
+            // 寫入 Outbox Table
+            var outboxMessage = new OutboxMessage
+            {
+                Id = Guid.NewGuid(),
+                Type = typeof(BasketCheckoutIntegrationEvent).AssemblyQualifiedName!,
+                Content = JsonSerializer.Serialize(eventMessage),
+                OccuredOn = DateTime.UtcNow
+            };
+            dbContext.OutboxMessages.Add(outboxMessage);
+            // Delete the basket
+            dbContext.ShoppingCarts.Remove(basket);
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
+            return new CheckoutBasketResult(true);
+        }
+        catch (System.Exception)
+        {
+            await transaction.RollbackAsync(cancellationToken);
+            return new CheckoutBasketResult(false);
+        }
     }
 }
